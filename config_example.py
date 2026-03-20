74c46e7418e94552c3f93dac18b93d8e11b6d787
seukeol <seukeolttyl@gmail.com>
2026-03-20 16:44:49 +0300

    config example

---
+++ config_example.py
@@ -0,0 +1,5 @@
+search = 'python backend'
+employment_forms = ['FULL', 'PART', 'PROJECT']
+work_formats = ['ON_SITE', 'REMOTE', 'HYBRID']
+items_on_page = 20 #эт лучше не менять
+pages_to_parse = 30
